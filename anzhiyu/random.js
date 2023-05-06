78b4dd4d56e1b172430c9f6ae4c417149495cdaf
沉珂 <2822168834@qq.com>
2023-05-06 23:15:35 +0800

    Site updated: 2023-05-06 23:15:35

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/4a17b156.html","posts/1de3cf88.html","posts/e043b629.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["posts/1de3cf88.html","posts/4a17b156.html","posts/e043b629.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
